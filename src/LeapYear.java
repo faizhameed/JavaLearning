2850f1a84b2b76e9078968bac95e3817e2d16087
Faiz <faizhameedv@gmail.com>
2024-03-05 13:56:56 +0530

    is LEa

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -1,4 +1,7 @@
 public class LeapYear {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(3600));
+    }
     public static boolean isLeapYear (int year){
         if(year<=0 || year%4!=0 || year>9999) return false;
         if(year%100!=0) return true;
