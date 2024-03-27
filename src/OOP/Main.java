bdb1fced17826bb64a5bad98dfa00636557c418b
Faiz <faizhameedv@gmail.com>
2024-03-27 22:02:47 +0530

    Constructor challenge

--- src/OOP/Main.java
+++ src/OOP/Main.java
@@ -10,5 +10,22 @@ public class Main {
 
         Car defaultCar =  new Car();
         defaultCar.describeCar();
+
+
+        // Create customer
+        Customer a1 = new Customer("Faiz","faiz@gmail.com");
+        System.out.println(a1.getCreditLimit());
+        System.out.println(a1.getName());
+        System.out.println(a1.getEmail());
+
+        Customer a2= new Customer("Art",1000,"art@gmail.com");
+        System.out.println(a2.getCreditLimit());
+        System.out.println(a2.getName());
+        System.out.println(a2.getEmail());
+
+        Customer a3= new Customer();
+        System.out.println(a3.getCreditLimit());
+        System.out.println(a3.getName());
+        System.out.println(a3.getEmail());
     }
 }
