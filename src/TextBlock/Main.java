54c98cb5de1c5a9042acc2197ceaf939f66b47bf
Faiz <faizhameedv@gmail.com>
2024-04-06 10:10:06 +0530

    Textblock in Java

---
+++ src/TextBlock/Main.java
@@ -0,0 +1,8 @@
+package TextBlock;
+
+public class Main {
+    public static void main(String[] args) {
+         String bulletit = "Print a bulleted List:\n"+"\u2022 This is the first point\n"+"\u2022 This is the second point\n"+"\t\u2022 This is a sub point\n";
+         System.out.println(bulletit);
+    }
+}
