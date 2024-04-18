6396115dfb9bf1edf91f12bf20321104f8a99a18
Faiz <faizhameedv@gmail.com>
2024-04-18 23:59:57 +0530

    excercise circle part 2

--- src/Circle.java
+++ src/Circle.java
@@ -1,7 +1,19 @@
 public class Circle {
-    public double radius;
+   private double radius;
 
     public Circle(double radius) {
+        if (radius < 0) {
+            this.radius = 0;
+        }
         this.radius = radius;
     }
+
+    public double getRadius(){
+        return  this.radius;
+    }
+
+    public double getArea(){
+        return  this.radius*this.radius*Math.PI;
+    }
 }
+
