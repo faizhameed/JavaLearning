81038af3d44330a346a815e2bd1c9d535b91bc51
Faiz <faizhameedv@gmail.com>
2024-12-07 13:32:57 +0530

    Composition lesson complete

--- src/ComputerFactory/PersonalComputer.java
+++ src/ComputerFactory/PersonalComputer.java
@@ -11,16 +11,25 @@ public class PersonalComputer extends  Product{
         this.monitor = monitor;
         this.motherboard = motherboard;
     }
+private void drawLogo(){
+        monitor.drawPixelAt(1200,50,"yellow");
+}
+public void powerUp(){
+        // we are hiding pressPowerButton and drawLogo being exposed out
+        computerCase.pressPowerButton();
+        drawLogo();
+}
+//    public ComputerCase getComputerCase() {
+//        return computerCase;
+//    }
+//
+//    public Monitor getMonitor() {
+//        return monitor;
+//    }
+//
+//    public Motherboard getMotherboard() {
+//        return motherboard;
+//    }
 
-    public ComputerCase getComputerCase() {
-        return computerCase;
-    }
 
-    public Monitor getMonitor() {
-        return monitor;
-    }
-
-    public Motherboard getMotherboard() {
-        return motherboard;
-    }
 }
