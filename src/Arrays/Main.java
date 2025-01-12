c6431eb3588738f3eaec463384b21d568cc360d8
Faiz <faizhameedv@gmail.com>
2025-01-12 21:05:23 +0530

    Arrays part 2

--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         //you can't change the size of the array after the array is instantiated.
@@ -11,12 +13,29 @@ public class Main {
         myDoubleArray[2] = 3.5;
         System.out.println(myDoubleArray[2]);
 
-        int[] first5 = {15,16,29,39,-40};
+        int[] first5 = {15, 16, 29, 39, -40};
         int arrayLen = first5.length;
-        System.out.println("length of array "+ arrayLen );
-        System.out.println("last elem " + first5[arrayLen-1]);
-        for(int i=0;i<arrayLen;i++){
-            System.out.printf("Array at position %d is %d %n",i,first5[i]);
+        System.out.println("length of array " + arrayLen);
+        System.out.println("last elem " + first5[arrayLen - 1]);
+        for (int i = 0; i < arrayLen; i++) {
+            System.out.printf("Array at position %d is %d %n", i, first5[i]);
         }
+        // alternative way to print with enhanced loop
+        int i = 0;
+        for (int elem : first5) {
+            System.out.printf("Array element at %d %d ", i, elem);
+            i++;
+        }
+        System.out.println(first5);// prints some
+        System.out.println("i " + i);
+        System.out.println(Arrays.toString(first5)); //Array util to print array values
+        // you can create object array to support any kind of values
+        Object[] objectArray = new Object[4];
+        objectArray[0] = "Faiz";
+        objectArray[1] = 5;
+        objectArray[2] = 1.001;
+        objectArray[3]= new StringBuilder("Hameed");
+        System.out.println(Arrays.toString(objectArray));
+
     }
 }
