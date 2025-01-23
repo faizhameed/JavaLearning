9665a23f08814e498e1db479c3793ebf6c27cee6
Faiz <faizhameedv@gmail.com>
2025-01-23 22:17:04 +0530

    unboxing and boxing examples

--- src/Autoboxing/Main.java
+++ src/Autoboxing/Main.java
@@ -7,15 +7,28 @@ public class Main {
 */
 //        Integer my_int =  new Integer(4); // deprecated! boxing
         Integer my_int = 6; // autoboxing
+        Integer my_int_2  =  Integer.valueOf(8);
         System.out.printf("my_int %d %n", my_int);
+        System.out.printf("my_int_2 %d %n", my_int_2);
         // Now my_int can be unboxed
         int unboxed_int = my_int; // int i = my_int.intValue(); can also be done
         System.out.printf("unboxed_int: %d",unboxed_int);
         Float my_float = 3.12F;
         System.out.printf("my float %.2f %n", my_float);
+        System.out.printf(my_float.getClass().getName()); // to get class name, but doesn't work on primitive types
+
+
 
         float unboxed_float = my_float ;
         System.out.printf("unboxed float: %.2f",unboxed_float);
 
+            Double resultBoxed = getLiteralDoublePrimitive(); // boxing occurs automatically
+            double  resultUnboxed =  getDoubleObject(); // unboxing occurs automaticallly
     }
+        private static double getDoubleObject(){
+            return Double.valueOf(100.0);
+        }
+        private static double getLiteralDoublePrimitive(){
+        return 100.0;
+        }
 }
