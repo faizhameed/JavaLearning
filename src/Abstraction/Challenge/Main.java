0a3feab7237f101f719f7113700b553e4e3f6e3b
Faiz <faizhameedv@gmail.com>
2025-02-09 19:42:25 +0530

    abstraction challenge part 3

--- src/Abstraction/Challenge/Main.java
+++ src/Abstraction/Challenge/Main.java
@@ -1,11 +1,17 @@
 package Abstraction.Challenge;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Store store = new Store();
-        store.addAnItemToTheOrder(new MobilePhone("Samsung",1299.00,"This is a quality phone"));
-        store.addAnItemToTheOrder(new MobilePhone("Iphone",1899.00,"Iphone with dynamic Island!"));
+        store.addAnItem(new MobilePhone("Samsung",1299.00,"This is a quality phone"));
+        store.addAnItem(new MobilePhone("Iphone",1899.00,"Iphone with dynamic Island!"));
 
         store.listProducts();
+        var order =  new ArrayList<OrderItem>();
+        store.addItemToOrder(order,1,3);
+        store.addItemToOrder(order,0,1);
+        store.printOrder(order);
     }
 }
--- src/Abstraction/Challenge/Store.java
+++ src/Abstraction/Challenge/Store.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Store {
     private ArrayList <ProductForSale> storeProducts = new ArrayList<>();
-      public void addAnItemToTheOrder(ProductForSale newProduct){
+      public void addAnItem(ProductForSale newProduct){
           boolean add = storeProducts.add(newProduct);
           if(add){
               System.out.println("Product added successfully!");
@@ -16,5 +16,16 @@ public class Store {
               item.showDetails();
           }
       }
+      public  void addItemToOrder(ArrayList<OrderItem> order, int orderIndex, int qty){
+          order.add(new OrderItem(qty,storeProducts.get(orderIndex)));
+      }
 
+    public void printOrder(ArrayList<OrderItem> order){
+          double salesTotal = 0;
+          for(var item:order){
+              item.product().printPricedItem(item.quantity());
+              salesTotal+=item.product().getSalesPrice(item.quantity());
+          }
+        System.out.printf("Sales Total = $%6.2f %n",salesTotal);
+    }
 }
