5d7aaccefa717cac361f08a13b61308c0869dadb
Faiz <faizhameedv@gmail.com>
2025-02-13 22:01:27 +0530

    java interfaces

--- src/Interfaces/Bird.java
+++ src/Interfaces/Bird.java
@@ -3,6 +3,29 @@ package Interfaces;
 public class Bird extends Animal implements Trackable,FlightEnabled{
     @Override
     public void move() {
+        System.out.println("Wings on flight!");
+    }
+
+    @Override
+    public void takeOff() {
+        System.out.println(getClass().getSimpleName()+" is taking off!");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(getClass().getSimpleName()+" is flying!");
+
+    }
+
+    @Override
+    public void land() {
+        System.out.println(getClass().getSimpleName()+" is landed");
+
+    }
+
+    @Override
+    public void track() {
+        System.out.println(getClass().getSimpleName()+" coordinates recorded!");
 
     }
 }
--- src/Interfaces/Main.java
+++ src/Interfaces/Main.java
@@ -3,5 +3,17 @@ package Interfaces;
 public class Main {
     public static void main(String[] args) {
 
+        Bird bird =  new Bird();
+        Animal animal = bird;
+        FlightEnabled flier =  bird;
+        Trackable tracked = bird;
+
+        animal.move();
+        flier.takeOff();
+        flier.fly();
+        flier.land();
+        tracked.track();
+//    flier.move(); // doesnt compile
+//    tracked.move(); // doesnt compile
     }
 }
