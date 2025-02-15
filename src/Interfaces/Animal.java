3a9f47f21d178f44edfedb7b858bcaf43bd9292d
Faiz <faizhameedv@gmail.com>
2025-02-15 22:13:49 +0530

    interface with arraylist and list

--- src/Interfaces/Animal.java
+++ src/Interfaces/Animal.java
@@ -1,5 +1,20 @@
 package Interfaces;
 
+enum FlightStages implements Trackable {
+    GROUNDED, LAUNCH, CRUISE, DATA_COLLECTION;
+
+    @Override
+    public void track() {
+        if (this != GROUNDED) {
+            System.out.println("Monitoring" + this);
+        }
+    }
+}
+
+interface OrbitEarth extends FlightEnabled {
+    void achieveOrbit();
+}
+
 interface FlightEnabled {
     double MILES_TO_KM = 1.60934;
     double KM_TO_MILES = 0.621371;
@@ -13,6 +28,45 @@ interface FlightEnabled {
 
 interface Trackable {
     void track();
+}
+
+record DragonFly(String name, String type) implements FlightEnabled {
+
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+}
+
+class Satellite implements OrbitEarth {
+    public void achieveOrbit() {
+        System.out.println("Orbit");
+    }
+
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
 }
 
 public abstract class Animal {
--- src/Interfaces/Main.java
+++ src/Interfaces/Main.java
@@ -1,5 +1,8 @@
 package Interfaces;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -12,8 +15,34 @@ public class Main {
         double kmsTraveled = 100;
         double milesTraveled = kmsTraveled * FlightEnabled.KM_TO_MILES;
         System.out.printf("The truck traveled %.2f km or %.2f miles%n", kmsTraveled, milesTraveled);
-    }
 
+        LinkedList<FlightEnabled> fliers =  new LinkedList<>();
+        List<FlightEnabled> betterFliers  =  new LinkedList<>();
+        fliers.add(bird);
+        betterFliers.add(bird);
+
+        triggerFlyFliers(fliers);
+        flyFliers(fliers);
+        landFliers(fliers);
+
+        triggerFlyFliers(betterFliers);
+        flyFliers(betterFliers);
+        landFliers(betterFliers);
+
+    }
+    private static void triggerFlyFliers(List<FlightEnabled> fliers){
+        for(var flier:fliers){
+            flier.takeOff();
+        }
+    } private static void flyFliers(List<FlightEnabled> fliers){
+        for(var flier:fliers){
+            flier.fly();
+        }
+    } private static void landFliers(List<FlightEnabled> fliers){
+        for(var flier:fliers){
+            flier.land();
+        }
+    }
     private static void inFlight(FlightEnabled flier) {
         flier.takeOff();
         flier.fly();
